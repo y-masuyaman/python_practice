916c0e7e81ced26a54959ad6a92271e0d836ae89
y-masuyaman <y-masuyama@tactinc.jp>
2020-10-20 19:07:52 +0900

    update

--- keisan_practice.py
+++ keisan_practice.py
@@ -22,15 +22,15 @@ def keisan(method):
     if method == '+':
         a = random.randint(1, 99)
         b = random.randint(1, 100 - a)
-        return a, b
+        return a, b, a + b
     elif method == '-':
         a = random.randint(1, 99)
         b = random.randint(1, 99)
-        return max([a, b]), min([a, b])
+        return max([a, b]), min([a, b]), abs(a - b)
     elif method == '*':
         a = random.randint(1, 99)
         b = random.randint(1, 99)
-        return a, b
+        return a, b, a * b
     elif method == '%':
         a = random.randint(1, 99)
         primes = trial_division(a)
@@ -42,25 +42,34 @@ def keisan(method):
             b = 1
             for i in prime_nums:
                 b *= i
-        return a, b
+        return a, b, a // b
 
 
 def main():
     for _ in range(20):
         temp = copy.deepcopy(calc_temp)
         method = random.choice(method_of_calc)
-        a, b = keisan(method)
+        a, b, c = keisan(method)
         temp[0] = str(a)
         temp[1] = method
         temp[2] = str(b)
         drill.append(temp)
-
-    for formula in drill:
-        print(' '.join(formula))
+        answers.append(c)
+    print('*' * 20)
+    print('Question')
+    print('_' * 20)
+    for i, formula in enumerate(drill, start=1):
+        print(i, ":", ' '.join(formula))
+    print('*' * 20)
+    print('Answer')
+    print('_' * 20)
+    for i, ans in enumerate(answers, start=1):
+        print(i, ":", ans)
 
 
 if __name__ == '__main__':
     drill = []
+    answers = []
     calc_temp = ['a', '+', 'b', '=']
     method_of_calc = ('+', '-', '*', '%')
     main()
